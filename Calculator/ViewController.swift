569de6788110c4b1318f84575e8b6381643d7fd3
Felix Prünte <f.pruente@xu-university.de>
2020-03-06 09:45:23 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by FelixP on 06.03.20.
+//  Copyright © 2020 XU Exponential University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
